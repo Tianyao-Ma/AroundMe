2273b234ce643e43ee9b10f26cca5b83d6809410
Tianyao Ma <tianyao.ma333@gmail.com>
2021-04-24 00:38:02 +0000

    implement search_by_keyWord and search_by_User methods using elasticsearch

--- main.go
+++ main.go
@@ -3,15 +3,16 @@ package main
 import (
     "fmt"
     "log"
-    "net/http"
+    "net/http"  
 
-    "github.com/gorilla/mux"
+    "github.com/gorilla/mux"  
 )
+
 func main() {
     fmt.Println("started-service")
 
     r := mux.NewRouter()
     r.Handle("/upload", http.HandlerFunc(uploadHandler)).Methods("POST", "OPTIONS")
+    r.Handle("/search", http.HandlerFunc(searchHandler)).Methods("GET", "OPTIONS")
     log.Fatal(http.ListenAndServe(":8080", r))
 }
-
